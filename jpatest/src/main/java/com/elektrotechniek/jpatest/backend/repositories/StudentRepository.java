02fddbb8c4e9e55180f2ce4f90eae9956e15b315
Dinesh H <dinesh_harkhoe@hotmail.com>
2020-12-17 12:48:13 -0300

    Update StudentRepository.java
    added query for search naam, achternaam & studnummer

--- jpatest/src/main/java/com/elektrotechniek/jpatest/backend/repositories/StudentRepository.java
+++ jpatest/src/main/java/com/elektrotechniek/jpatest/backend/repositories/StudentRepository.java
@@ -2,7 +2,14 @@ package com.elektrotechniek.jpatest.backend.repositories;
 
 import com.elektrotechniek.jpatest.backend.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-
+    @Query(value = "SELECT * FROM elektrotechniek.student WHERE (naam LIKE CONCAT('%', :search, '%')" +
+            "OR achternaam LIKE CONCAT('%', :search, '%')" +
+            "OR studentennummer LIKE CONCAT(:search, '%'))", nativeQuery = true)
+    List<Student> search(@Param("search") String searchText);
 }
